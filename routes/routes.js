50a2dc231ac5629ce978cc7b16a93c48478f4ed0
obed vega <vega.obed@gmail.com>
2020-09-10 22:21:30 -0700

    ejs added

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 router.get('/', (req, res) =>{
-    res.send('Index');
+    //res.send('Index');
+    res.render('pages/index');
 });
 
 router.get('/about', function(req, res) {
     res.sendFile('about.html', { root: 'public' });
+//    res.render('pages/about')
 });
 
-router.get('/contact', (req, res) =>{
-    res.send('Contact Us');
+router.get('/contact', function(req, res) {
+    res.render('pages/contact');
+    //res.send('Contact Us');
 });
 
 module.exports = router
